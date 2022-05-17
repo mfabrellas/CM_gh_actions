4b6378156d1e4a116dcc4d77552c939800492f59
miguel.fabrellas <miguel.fabrellas.contractor@bbva.com>
2022-05-17 11:49:03 +0200

    Add provider.tf and Edit workflow terraform.yaml

---
+++ variables.tf
@@ -0,0 +1,13 @@
+#Genesys Cloud Credentials
+
+variable "GENESYSCLOUD_CLIENT_ID" {
+    type = string
+    description = "GenesysCloud ClientId credential"
+    sensitive = true
+}
+
+variable "GENESYSCLOUD_CLIENT_SECRET" {
+    type = string
+    description = "GenesysCloud ClientSecret credential"
+    sensitive = true
+}
