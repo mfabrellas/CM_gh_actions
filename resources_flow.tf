4d4a48a80b0f816f096b9247e6439f1714d04847
miguel.fabrellas <miguel.fabrellas.contractor@bbva.com>
2022-05-31 11:02:56 +0200

    remove example

--- resources_flow.tf
+++ resources_flow.tf
@@ -1,20 +1,20 @@
-#Resource to create example Inbound Call Flow in genesys cloud
-resource "genesyscloud_flow" "IntentBotExample" {
-  filepath = "./example/IntentBotExample.yaml"
-  substitutions = {
-      example_flow_name = var.example_flow_name
-      division = var.division
-      default_language = var.default_language
+# #Resource to create example Inbound Call Flow in genesys cloud
+# resource "genesyscloud_flow" "IntentBotExample" {
+#   filepath = "./example/IntentBotExample.yaml"
+#   substitutions = {
+#       example_flow_name = var.example_flow_name
+#       division = var.division
+#       default_language = var.default_language
 
-      queue_name = var.queue_name
+#       queue_name = var.queue_name
       
-      #call common module
-      common_module_name = var.common_module_name
-  }
-  depends_on = [
-    genesyscloud_flow.IntentClientModuleIntermediate   
-  ]
-}
+#       #call common module
+#       common_module_name = var.common_module_name
+#   }
+#   depends_on = [
+#     genesyscloud_flow.IntentClientModuleIntermediate   
+#   ]
+# }
 
 #Resource to create Common Module Flow in genesys cloud
 resource "genesyscloud_flow" "IntentClientModuleIntermediate" {
